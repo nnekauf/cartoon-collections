00b7f8b20bd815cdc098b6d4551ab74a9a0879aa
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-20 01:39:25 -0500

    commit tree bfd5f603c2481959eeaa995b894e872aa71222c4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index {|name, position| puts "#{position+1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteer_calls)
+  planteer_calls.collect{|word| word.capitalize+"!"}
+    
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any?{|n| n.length> 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(list)
+ 
+cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  list.detect{|item| cheese_types.include?(item)}
+  
 end
+
